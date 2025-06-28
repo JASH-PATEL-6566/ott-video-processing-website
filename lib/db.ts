45e83f15b65539db295d46850921fafcc113b948
JASH-PATEL-6566 <pateljash1511.com>
2025-06-28 20:38:50 -0300

    change name of env

--- lib/db.ts
+++ lib/db.ts
@@ -2,10 +2,10 @@ import mysql from "mysql2/promise";
 
 // Create a connection pool to the MySQL database
 export const pool = mysql.createPool({
-  host: process.env.MYSQL_HOST || "localhost",
-  user: process.env.MYSQL_USER || "root",
-  password: process.env.MYSQL_PASSWORD || "",
-  database: process.env.MYSQL_DATABASE || "video_processor",
+  host: process.env.NEXT_PUBLIC_MYSQL_HOST || "localhost",
+  user: process.env.NEXT_PUBLIC_MYSQL_USER || "root",
+  password: process.env.NEXT_PUBLIC_MYSQL_PASSWORD || "",
+  database: process.env.NEXT_PUBLIC_MYSQL_DATABASE || "video_processor",
   waitForConnections: true,
   connectionLimit: 10,
   queueLimit: 0,
